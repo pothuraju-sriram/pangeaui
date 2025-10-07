05a49cf83c1951032db1970df9c76575ddf7c7a4
pothuraju-sriram <ram.k160796@gmail.com>
2025-10-07 17:35:30 +0530

    Coming soon page

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ComingSoon />
+  </div>
+</template>
+<style>
+body {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  margin: 0;
+  background-color: #1e1e1e;
+}
+</style>
