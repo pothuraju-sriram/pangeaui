d05fb951ded8edf78305cca3eb8b1d12528b24da
pothuraju-sriram <ram.k160796@gmail.com>
2025-10-09 14:04:07 +0530

    upgrade to nuxt 4.1.3

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <UPageHero headline="Launching soon - your village, reimagined." title="Gramam - Where Villages Go Digital"
+      description="We're building a connected future for rural India. From local food spots to village news, services, and essentials - everything you need, just a tap away. Smart. Simple. Sustainable." />
+  </div>
+</template>
